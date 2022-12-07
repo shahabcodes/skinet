080b3b920f3fb8fcea03cb82efc332fd7bbc8d1b
shahabcodes <shahabusaid.mm@gmail.com>
2022-12-07 19:14:26 +0300

    END of Section 5 -

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -1,4 +1,5 @@
 using API.Errors;
+using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Logg;
@@ -10,12 +11,16 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
+
+            services.AddSingleton<StoreContext>();
             /* AddScoped Creates an instance of the ProductRepository when API receives 
             an HTTP request and Create a new instance of the Controllers, disposes it later */
             services.AddScoped<IProductRepository, ProductRepository>();
-
+            //Adding auto Mapper (Nuget Package for concatenating URLs received from API requests)
+            services.AddAutoMapper(typeof(Mapper));  // Could slow your application 
             // Adding Generic Repository
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddEndpointsApiExplorer();
 
             services.AddScoped<ILoggerManager, LoggerManager>();
 
--- API/Program.cs
+++ API/Program.cs
@@ -9,17 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
 // Add services to the container.
-builder.Services.AddSingleton<StoreContext>();
-
-//Adding auto Mapper (Nuget Package for concatenating URLs received from API requests)
-builder.Services.AddAutoMapper(typeof(Mapper));  // Could slow your application
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddApplicationServices();
 
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerDocumentation();
 
 builder.Services.Configure<ConfigureConnection>
@@ -31,11 +25,11 @@ app.UseMiddleware<ExceptionMiddleware>();
 //Using Swagger for both development and production
 app.UseSwaggerDocumentation();
 // // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
+// //if (app.Environment.IsDevelopment())
+//// {
+////     app.UseSwagger();
+////     app.UseSwaggerUI();
+//// }
 
 // Redirect to Error controller when endpoint doesn't exist
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
